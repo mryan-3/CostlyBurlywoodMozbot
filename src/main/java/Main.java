122e3d879fdf6a0dc347475ae4d90f6f633449bd
Ryan Malawa <113714055+mryan-3@users.noreply.github.com>
2024-03-01 10:05:47 +0000

    Fizz Buzz program in Java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,25 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
-import java.text.NumberFormat;
+
 import java.util.Scanner;
 
 public class Main {
   
   public static void main(String[] args) {  
     Scanner scanner = new Scanner(System.in);
-    System.out.print("Principal: ");
-    int principal = scanner.nextInt();
-    System.out.print("Annual Interest Rate: ");
-    float annualInterestRate = scanner.nextFloat();
-    System.out.print("Period (Years): ");
-    int period = scanner.nextInt();
+    System.out.print("Number: ");
+    int number = scanner.nextInt();
 
-    float monthlyInterestRate = annualInterestRate /1200;
-
-    int numberOfPayments = period * 12;
-    
-    float mortgage = principal * (monthlyInterestRate * (float)Math.pow(1 + monthlyInterestRate, numberOfPayments)/(float)Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1);
-    String mortgageFormatted = NumberFormat.getCurrencyInstance().format(mortgage);
-    System.out.print("Mortgage: " + mortgageFormatted);
+    if (number % 5 == 0 && number % 3 == 0) {
+      System.out.println("FizzBuzz");
+    }  else if (number % 3 == 0) {
+      System.out.println("Buzz");
+    } else if (number % 5 == 0) {
+      System.out.println("Fizz");
+    } else {
+      System.out.println(number);
+    }
+   
 }
 } 
