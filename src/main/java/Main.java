8a1bcdd74301775cbb743f0ee8d46841784dd4b0
Ryan Malawa <113714055+mryan-3@users.noreply.github.com>
2024-03-11 16:26:05 +0000

    classes and extending of classes. Inheritance

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,22 +4,24 @@
 
 import java.util.Scanner;
 
+
+class Animal {
+    public void eat() {
+      System.out.println("Animal is eating");
+    }  
+  }
+class Pig extends Animal{
+  public void eat () {
+    System.out.println("Pig is eating");
+  }
+}
 public class Main {
   
   public static void main(String[] args) {  
-    Scanner scanner = new Scanner(System.in);
-    System.out.print("Number: ");
-    int number = scanner.nextInt();
-
-    if (number % 5 == 0 && number % 3 == 0) {
-      System.out.println("FizzBuzz");
-    }  else if (number % 3 == 0) {
-      System.out.println("Buzz");
-    } else if (number % 5 == 0) {
-      System.out.println("Fizz");
-    } else {
-      System.out.println(number);
-    }
-   
-}
+    Animal myAnimal = new Animal();
+    Animal myPig = new Pig();
+    myPig.eat();
+    myAnimal.eat();
 } 
+ 
+}
